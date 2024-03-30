cd56131ec598ad95956e2aaf84ab95892c5fd6f3
skyfire <skyfireitdiy@hotmail.com>
2024-03-30 14:53:05 +0000

    修复脚本

--- install_codeql.sh
+++ install_codeql.sh
@@ -4,7 +4,7 @@ if [ $# -ge 1 ]; then
     version=$1
 fi
 echo will install codeql version:$version
-wget https://github.com/github/codeql-action/releases/download/codeql-bundle-v2.16.4/codeql-bundle.tar.gz
+wget https://github.com/github/codeql-action/releases/download/codeql-bundle-$version/codeql-bundle-linux64.tar.gz
 tar xvf codeql-bundle.tar.gz
 mv codeql codeql-$version
 sudo ln -sf `pwd`/codeql-$version/codeql /usr/bin/codeql 
